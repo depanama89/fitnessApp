5416666e2910ea6a5a67f938c660460f56a74fe6
Patrick Kudiatu <kudiatutomanitu@gmail.com>
2025-05-23 06:44:51 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:fitness/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fitness & workout',
+      theme: ThemeData(primaryColor: Color(0xFF1C1C1E)),
+      home: HomeScreen(),
+    );
+  }
+}
